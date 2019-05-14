357dc56231ad33b4f0db8679ec62f8f070348911
JuanDanielVillegas <45501165+JuanDanielVillegas@users.noreply.github.com>
2019-05-13 18:43:25 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 #include <windows.h>
-
+#include <string>
 using namespace std;
+
+int ValidarEntrada();
+bool isValidInt(string numeroEnCadena);
 int opci = 0;
 
 void introduc (string nomb_usu){
@@ -14,7 +17,6 @@ void introduc (string nomb_usu){
     <<"\n2. Llorar y no hacer nada";
     cin>>opci;
 }
-
 
 void nombre(){
     string nombre;
@@ -38,3 +40,62 @@ int main()
 
     return 0;
 }
+
+
+int ValidarEntrada() {
+
+	string eleccionCadena;
+	bool key = false;
+	int eleccionEntero = 0;
+	
+	do {
+		cin >> eleccionCadena;
+		int validacion = isValidInt(eleccionCadena);
+		try {
+			if (validacion != 0) {
+				string::size_type sz;
+				eleccionEntero = stoi(eleccionCadena, &sz);
+				if (eleccionEntero >= 1 && eleccionEntero <= 2) {
+					return 1;
+				} 
+				else {
+					throw 1;
+				}
+			}
+			else {
+				throw "Ingrese un entero";
+			}
+		}
+		catch (const char* ErrorUno) {
+			cout << ErrorUno << endl;
+		}
+		catch (int ErrorDos) {
+			cout << "Seleccione la opcion 1 o 2" << endl;
+		}
+	} while (key != true);
+}
+
+bool isValidInt(string numeroEnCadena) {
+	int inicio = 0;
+	int i = 0;
+	bool valido = true;
+	bool signo = false;
+
+	int tamanioDeCadena = numeroEnCadena.size();
+
+	if (tamanioDeCadena == 0)
+		valido = 0;
+	if (numeroEnCadena.at(0) == '-' || numeroEnCadena.at(0) == '+') {
+		inicio = 1;
+		signo = true;
+	}
+	if (signo && tamanioDeCadena == 1)
+		valido = false;
+
+	for (i = inicio; valido && i < tamanioDeCadena; i++) {
+		if (!isdigit(numeroEnCadena.at(i)))
+			valido = false;
+	}
+	return valido;
+}
+
